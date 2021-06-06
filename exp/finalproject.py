5b9b0062b7e4a992c4959139e34afd332ba636e9
Linda Luo <lindaluo@lindas-air.attlocal.net>
2021-06-06 07:27:19 -0500

    finished

--- exp/finalproject.py
+++ exp/finalproject.py
@@ -1,10 +1,7 @@
 """
 Experiment summary
 ------------------
-Treat each province/state in a country cases over time
-as a vector, do a simple K-Nearest Neighbor between 
-countries. What country has the most similar trajectory
-to a given country?
+Predict future cases
 """
 
 import sys
@@ -26,7 +23,7 @@ MIN_CASES = 1000
 # ------------------------------------------
 
 confirmed = os.path.join(
-    BASE_PATH, 
+    BASE_PATH,
     'csse_covid_19_time_series',
     'time_series_covid19_confirmed_global.csv')
 confirmed = data.load_csv_data(confirmed)
@@ -58,12 +55,25 @@ for val in np.unique(confirmed["Country/Region"]):
         confirmed, "Country/Region", val)
     cases, labels = data.get_cases_chronologically(df)
     cases = cases.sum(axis=0)
+    time_after_jan = range(0, len(cases))
+    for i in range(len(cases)):
+        if cases[i] == 0:
+            cases[i] = .00000001
+
+    bestfit = np.polyfit(time_after_jan, np.log(cases.astype(float)), 2)
+    bestfitpoly = np.poly1d(bestfit)
+    numdays_data = len(cases)
+    for i in range(numdays_data,numdays_data+100): #predict next cases
+        cases=np.append(cases, np.exp(bestfitpoly(i)))
+        # cases = np.append(cases, pow(10,bestfitpoly(i)))
 
     if cases.sum() > MIN_CASES:
         i = len(legend)
         lines = ax.plot(cases, label=labels[0,1])
         handles.append(lines[0])
         lines[0].set_linestyle(LINE_STYLES[i%NUM_STYLES])
+        if i>=len(colors):
+            i=0
         lines[0].set_color(colors[i])
         legend.append(labels[0, 1])
 
@@ -73,4 +83,4 @@ ax.set_xlabel("Time (days since Jan 22, 2020)")
 ax.set_yscale('log')
 ax.legend(handles, legend, bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=4)
 plt.tight_layout()
-plt.savefig('results/cases_by_country.png')
+plt.savefig('results/predictions_by_country.png')
